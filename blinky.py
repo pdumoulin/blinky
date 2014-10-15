a7c281593feab1f97961567d6866d3154346ca1d
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2014-10-14 22:21:03 -0400

    refactor into a switch class

--- blinky.py
+++ blinky.py
@@ -12,34 +12,101 @@ except ImportError:
     import sys
     droid = None
 
-ports = [49153, 49152, 49154]
-commands = {
-    'on' : {
-        'body'   : '<u:SetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"><BinaryState>1</BinaryState></u:SetBinaryState>',
-        'header' : '"urn:Belkin:service:basicevent:1#SetBinaryState"',
-        'data'   : 'BinaryState'
-    },
-    'off' : {
-        'body'   : '<u:SetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"><BinaryState>0</BinaryState></u:SetBinaryState>',
-        'header' : '"urn:Belkin:service:basicevent:1#SetBinaryState"',
-        'data'   : 'BinaryState'
-    },
-    'status' : {
-        'body'   : '<u:GetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"><BinaryState>1</BinaryState></u:GetBinaryState>',
-        'header' : '"urn:Belkin:service:basicevent:1#GetBinaryState"',
-        'data'   : 'BinaryState'
-    },
-    'name' : {
-        'body'   : '<u:GetFriendlyName xmlns:u="urn:Belkin:service:basicevent:1"><FriendlyName></FriendlyName></u:GetFriendlyName>',
-        'header' : '"urn:Belkin:service:basicevent:1#GetFriendlyName"',
-        'data'   : 'FriendlyName'
-    },
-    'signal' : {
-        'body'   : '<u:GetSignalStrength xmlns:u="urn:Belkin:service:basicevent:1"><GetSignalStrength>0</GetSignalStrength></u:GetSignalStrength>',
-        'header' : '"urn:Belkin:service:basicevent:1#GetSignalStrength"',
-        'data'   : 'SignalStrength'
-    }
-}
+
+class wemo:
+    OFF_STATE = '0'
+    ON_STATE = '1'
+    ip = None
+    ports = [49153, 49152, 49154]
+    def __init__(self, switch_ip):
+        self.ip = switch_ip      
+   
+    # TODO - generate XML by (method, object) in order to DRY it up
+   
+    def toggle(self):
+        status = self.status()
+        if status == self.ON_STATE:
+            result = self.off()
+        elif status == self.OFF_STATE:
+            result = self.on()
+        else:
+            raise Exception("UnexpectedStatusResponse")
+        return result    
+    
+    def burst(self, seconds):
+        status = self.status()
+        if status == self.OFF_STATE:
+            self.on()
+            time.sleep(seconds)
+            result = self.off()
+            return result
+        else:
+            raise Exception("UnexpectedStatusResponse")
+    
+    def on(self):
+        return self._send(
+            '<u:SetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"><BinaryState>1</BinaryState></u:SetBinaryState>', 
+            '"urn:Belkin:service:basicevent:1#SetBinaryState"', 
+            'BinaryState'
+        )
+
+    def off(self):
+        return self._send(
+            '<u:SetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"><BinaryState>0</BinaryState></u:SetBinaryState>', 
+            '"urn:Belkin:service:basicevent:1#SetBinaryState"',
+            'BinaryState'
+        )
+
+    def status(self):
+        return self._send(
+            '<u:GetBinaryState xmlns:u="urn:Belkin:service:basicevent:1"><BinaryState>1</BinaryState></u:GetBinaryState>',
+            '"urn:Belkin:service:basicevent:1#GetBinaryState"',
+            'BinaryState'
+        )
+    
+    def name(self):
+        return self._send(
+            '<u:GetFriendlyName xmlns:u="urn:Belkin:service:basicevent:1"><FriendlyName></FriendlyName></u:GetFriendlyName>',
+            '"urn:Belkin:service:basicevent:1#GetFriendlyName"',
+            'FriendlyName'
+        )   
+
+    def signal(self):
+        return self._send(
+            '<u:GetSignalStrength xmlns:u="urn:Belkin:service:basicevent:1"><GetSignalStrength>0</GetSignalStrength></u:GetSignalStrength>',
+            '"urn:Belkin:service:basicevent:1#GetSignalStrength"',
+            'SignalStrength'
+        )
+    
+    def _send(self, body, header, data):
+        for port in self.ports:
+            result = self._try_send(self.ip, port, body, header, data) 
+            if result is not None:
+                self.ports = [port]
+            return result
+        raise Exception("TimeoutOnAllPorts")
+
+    def _try_send(self, ip, port, body, header, data):
+        try:
+            request = urllib2.Request('http://%s:%s/upnp/control/basicevent1' % (ip, port))
+            request.add_header('Content-type', 'text/xml; charset="utf-8"')
+            request.add_header('SOAPACTION', header)
+            request_body = '<?xml version="1.0" encoding="utf-8"?>'
+            request_body += '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'
+            request_body += '<s:Body>%s</s:Body></s:Envelope>' % body
+            request.add_data(request_body)
+            result = urllib2.urlopen(request, timeout=1)
+            return self._extract(result.read(), data)
+        except Exception as e:
+            print str(e)
+            return None
+
+    def _extract(self, response, name):
+        exp = '<%s>(.*?)<\/%s>' % (name, name)
+        g = re.search(exp, response)
+        if g:
+            return g.group(1)
+        return response
 
 def get_args():
     result = {}
@@ -57,62 +124,21 @@ def output(message):
     else:
         droid.makeToast(message)
 
-def send(ip, command):
-    global ports
-    for port in ports:
-        result = try_send(ip, command, port) 
-        if result is not None:
-            ports = [port]
-        return result
-    raise Exception("TimeoutOnAllPorts")
-
-def try_send(ip, command, port):
-    try:
-        request = urllib2.Request('http://%s:%s/upnp/control/basicevent1' % (ip, port))
-        request.add_header('Content-type', 'text/xml; charset="utf-8"')
-        request.add_header('SOAPACTION', commands[command]['header'])
-        body = '<?xml version="1.0" encoding="utf-8"?>'
-        body += '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'
-        body += '<s:Body>%s</s:Body></s:Envelope>' % commands[command]['body']
-        request.add_data(body)
-        result = urllib2.urlopen(request, timeout=1)
-        return extract(result.read(), commands[command]['data'])
-    except Exception as e:
-        return None
-
-def extract(response, name):
-    exp = '<%s>(.*?)<\/%s>' % (name, name)
-    g = re.search(exp, response)
-    if g:
-        return g.group(1)
-    return None
-
 def main():
     args = get_args()
-    if args['command'] in commands:
-        result = send(args['ip'], args['command'])
-        output(result)
+    switch = wemo(args['ip'])
+    if args['command'] == 'on':
+        output(switch.on())
+    elif args['command'] == 'off':
+        output(switch.off())
     elif args['command'] == 'toggle':
-        status = send(args['ip'], 'status')
-        if status == '1':
-            result = send(args['ip'], 'off')
-        elif status == '0':
-            result = send(args['ip'], 'on')
-        else:
-            raise Exception("UnexpectedStatusResponse")
-        output(result)
-    elif args['command'] == 'burst':
-        status = send(args['ip'], 'status')
-        if status == '1':
-            raise Exception("SwitchAlreadyOn")
-        elif status == '0':
-            result = send(args['ip'], 'on')
-            time.sleep(args['time'])
-            result = send(args['ip'], 'off')
-        else:
-            raise Exception("UnexpectedStatusResponse")
-    else:
-        raise Exception("InvalidCommand")
-
+        output(switch.toggle())
+    elif args['command'] == 'status':
+        output(switch.status())
+    elif args['command'] == 'name':
+        output(switch.name())
+    elif args['command'] == 'signal':
+        output(switch.signal())
+    
 if __name__ == "__main__":
     main()
