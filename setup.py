aa6bb8f88a32dd2adf661d90b9ec8aa0c19aca84
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2016-07-22 01:41:20 +0000

    setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Blinky',
+    version='1.0',
+    description='wemo switch control',
+    author='Paul Dumoulin',
+    author_email='paul.l.dumoulin@gmail.com',
+    url='https://github.com/pdumoulin/blinky'
+    )
